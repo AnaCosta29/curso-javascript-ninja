2886784198fb8ae7f3138ca5741ef56bcd079a27
AnaCosta29 <35525950+AnaCosta29@users.noreply.github.com>
2021-06-18 21:59:08 -0300

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -35,31 +35,38 @@ Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
 // ?
-
+console.log(`O primeiro array tem ${itemVariados.length} itens.`)
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
 // ?
-
+console.log(`O segundo array tem ${itemVariados[0].length} itens.`)
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
 
-i=10 
-while(i<=20){
-  if(i%2===0){
-    console.log( `Números pares entre 10 e 20: ${i}` );
-    i++
-  }
+let i = 10
+while (i <= 20) {
+  if(i%2===0)
+  console.log(`Números pares entre 10 e 20:${i}`);
+  i++
+
 }
     // ?
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
-console.log( 'Números ímpares entre 10 e 20:' );
+let j = 10
+while (j <= 20) {
+  if(j%2===1)
+  console.log(`Números ímpares entre 10 e 20: ${j}`);
+  j++
+
+}
+
 // ?
 
 /*
@@ -68,8 +75,14 @@ Só vamos mudar o range:
 - No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
-console.log( 'Números pares entre 100 e 120:' );
+for(let i=100;i<=120;i++){
+   if(i%2===0)
+console.log(`Números pares entre 100 e 120:${i}`);
+}
 // ?
 
-console.log( 'Números ímpares entre 111 e 125:' );
+for(let i=111;i<=125;i++){
+   if(i%2===1)
+console.log(`Números ímpares entre 111 e 125:${i}`);
+}
 // ?
