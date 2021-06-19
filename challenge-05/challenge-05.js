132faf48070dc75506073c89c40becebdba6b6f8
AnaCosta29 <35525950+AnaCosta29@users.noreply.github.com>
2021-06-18 21:07:17 -0300

    Create challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,17 +3,21 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 // ?
-
+let valueRandom=['ana',32,'estressada',null,{}]
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
+let getValueRandom=(value)=>{
+  return value
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
-
+getValueRandom(valueRandom[1])
+32
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
@@ -21,18 +25,26 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 // ?
-
+let setValue=(arr,number)=>{
+  return arr[number]
+}
+setValue(valueRandom,4)
+{}
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
-
+let valuedifferent=[0,null,pessoa={},'salario',[1,2,3,4]]
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
-
+console.log(setValue(valuedifferent,0))
+console.log(setValue(valuedifferent,1))
+console.log(setValue(valuedifferent,2))
+console.log(setValue(valuedifferent,3))
+console.log(setValue(valuedifferent,4))
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
@@ -48,19 +60,43 @@ propriedades:
 os livros.
 */
 // ?
+let book =(nameBook)=>{
+  let library={
+    'Nao fale com estranho':{
+     quantidadePaginas:456,
+     autor:'Harlan Coben',
+     editora:'Arqueiro',
+     },
+      'O menino de ouro':{
+      quantidadePaginas:416,
+      autor:'Abigail Tarttelin',
+      editora:'Todavia',
+      },
+      'Harry Potter':{
+      quantidadePaginas:299,
+      autor:'J. K. Rowling',
+      editora:'Bloomsbury Publishing Plc',
+      },
+    console.log(`O livro ${[nameBook]} tem ${[nameBook].quantidadePaginas} páginas!`)
+  }
+    if(!nameBook){
+    return library
+  }
+   return book[nameBook]
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
 // ?
-
+book()
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
 // ?
-
+console.log(`O livro ${book('Nao fale com estranho'} tem ${[nameBook].quantidadePaginas} páginas!`)
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
